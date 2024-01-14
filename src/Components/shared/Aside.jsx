239dcb25ed1748908c1b839e37c245cab38492d3
Alaa Yasser <alaayasser2018@gmail.com>
2024-01-14 09:50:51 +0200

    Add new icons and links to the Aside component

--- src/Components/shared/Aside.jsx
+++ src/Components/shared/Aside.jsx
@@ -9,6 +9,8 @@ import {
   FarmIcon,
   EmployeeIcon,
   ActivityIcon,
+  SettingIcon,
+  LogoutIcon,
 } from "../../assets/icons/icons";
 
 const Aside = () => {
@@ -16,46 +18,62 @@ const Aside = () => {
 
   return (
     <>
-      <div className={styles.aside__title}>
-        <span className={styles.aside__logo}>
-          <i>
-            <LogoIcon />
+      <div className={styles.aside__container}>
+        <div className={styles.aside__title}>
+          <span className={styles.aside__logo}>
+            <i>
+              <LogoIcon />
+            </i>
+            <h1>Cattlem</h1>
+          </span>
+          <i
+            className={styles.aside__double_arrow}
+            onClick={() => dispatch(toggleAside())}
+          >
+            <DoubleArrowIcon />
           </i>
-          <h1>Cattlem</h1>
-        </span>
-        <i
-          className={styles.aside__double_arrow}
-          onClick={() => dispatch(toggleAside())}
-        >
-          <DoubleArrowIcon />
-        </i>
+        </div>
+        <ul className={styles.aside__links}>
+          <li className={styles.aside__dashboard_link}>
+            <i>
+              <DashboardIcon />
+            </i>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <i>
+              <FarmIcon />
+            </i>
+            <span>Farm</span>
+          </li>
+          <li>
+            <i>
+              <EmployeeIcon />
+            </i>
+            <span>Employee</span>
+          </li>
+          <li>
+            <i>
+              <ActivityIcon />
+            </i>
+            <span>Activity</span>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.aside__links}>
-        <li className={styles.aside__dashboard_link}>
-          <i>
-            <DashboardIcon />
-          </i>
-          <span>Dashboard</span>
-        </li>
-        <li>
-          <i>
-            <FarmIcon />
-          </i>
-          <span>Farm</span>
-        </li>
-        <li>
-          <i>
-            <EmployeeIcon />
-          </i>
-          <span>Employee</span>
-        </li>
-        <li>
-          <i>
-            <ActivityIcon />
-          </i>
-          <span>Activity</span>
-        </li>
-      </ul>
+      <ul className={`${styles.aside__links} ${styles.aside__settings}`}>
+          <li>
+            <i>
+              <SettingIcon />
+            </i>
+            <span>Settings</span>
+          </li>
+          <li>
+            <i>
+              <LogoutIcon />
+            </i>
+            <span>Log Out</span>
+          </li>
+        </ul>
     </>
   );
 };
