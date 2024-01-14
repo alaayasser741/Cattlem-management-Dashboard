0197b8e71f67ba704acc3fc6eae60c76a59c9a4c
Alaa Yasser <alaayasser2018@gmail.com>
2024-01-14 10:22:20 +0200

    Add loading spinner to Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/dashboard.module.css";
 import Aside from "../Components/shared/Aside";
 import Nav from "../Components/shared/Nav";
@@ -6,42 +6,65 @@ import Activity from "../Components/Activity";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleAside } from "../store/layoutSlice";
 
+
 const Dashboard = () => {
   const isAsideOpen = useSelector((state) => state.layoutAside.isAsideOpen);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <main
-      className={
-        isAsideOpen
-          ? `${styles.dashboard__layout_hide_aside} ${styles.dashboard__layout}`
-          : `${styles.dashboard__layout}`
-      }
-    >
-      <aside
-        className={
-          isAsideOpen
-            ? `${styles.dashboard__aside}`
-            : `${styles.dashboard__aside_hide}`
-        }
-      >
-        <Aside />
-      </aside>
-      <nav className={styles.dashboard__nav}>
-        <Nav />
-      </nav>
-      <section className={isAsideOpen ? `${styles.dashboard__body} ${styles.remove_margin}`:`${styles.dashboard__body}`}>
-        <Activity />
-      </section>
-      <div
-        onClick={() => dispatch(toggleAside())}
-        className={
-          isAsideOpen
-            ? `${styles.overlay_show} ${styles.overlay}`
-            : `${styles.overlay}`
-        }
-      />
-    </main>
+    <>
+      {loading ? (
+        <div className={styles.dashboard__loader}>
+          <div className={styles.loader} />
+        </div>
+      ) : (
+        <main
+          className={
+            isAsideOpen
+              ? `${styles.dashboard__layout_hide_aside} ${styles.dashboard__layout}`
+              : `${styles.dashboard__layout}`
+          }
+        >
+          <aside
+            className={
+              isAsideOpen
+                ? `${styles.dashboard__aside}`
+                : `${styles.dashboard__aside_hide}`
+            }
+          >
+            <Aside />
+          </aside>
+          <nav className={styles.dashboard__nav}>
+            <Nav />
+          </nav>
+          <section
+            className={
+              isAsideOpen
+                ? `${styles.dashboard__body} ${styles.remove_margin}`
+                : `${styles.dashboard__body}`
+            }
+          >
+            <Activity />
+          </section>
+          <div
+            onClick={() => dispatch(toggleAside())}
+            className={
+              isAsideOpen
+                ? `${styles.overlay_show} ${styles.overlay}`
+                : `${styles.overlay}`
+            }
+          />
+        </main>
+      )}
+    </>
   );
 };
 
